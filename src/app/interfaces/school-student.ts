73ae37cdb11f987f2aea9d7b9ca55664333b4a5d
manuelarochap <135547387+manuelarochap@users.noreply.github.com>
2025-05-21 11:27:44 -0300

    Delete src/app/interfaces/school-student.ts

--- src/app/interfaces/school-student.ts
+++
@@ -1,9 +0,0 @@
-import { Grades } from './grades';
-
-export interface SchoolStudent{
-  id: string;
-  school_id: string;
-  student_id: string;
-  // grades: Grades;
-  fos: string; //field of study
-}
